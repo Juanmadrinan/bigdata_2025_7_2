9e307e7158bf9b9586e3d342db70491463e18414
juanmadrinan <jmadrinanpinzon@gmail.com>
2025-05-15 23:36:01 -0500

    Entregables y Setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="exchange_rate"
+    author="Juan Madriñan"
+    author_email="jmadrinanpinzon@gmail.com"
+    description=""
+    install_requires=[
+        "json",
+        "requests",
+        "pandas"
+    ]
+)
