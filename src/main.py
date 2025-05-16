dd7310248dcaa7bd30ad50dc958e9e6129c4a5ca
juanmadrinan <jmadrinanpinzon@gmail.com>
2025-05-16 16:11:01 -0500

    correcion path

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ def main():
     datapi = DatApi()
     database = Database()
     df = datapi.getData()
-    df.to_csv('bigdata_2025_7_2/src/static/csv/tasas_de_cambio.csv', index=False, encoding='utf-8')
+    df.to_csv('src/static/csv/tasas_de_cambio.csv', index=False, encoding='utf-8')
     database.close_database()
 
 if __name__ == "__main__":
