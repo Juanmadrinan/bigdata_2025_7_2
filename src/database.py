09cf836c9941b849cdb123cc30bf4af889b8bada
juanmadrinan <jmadrinanpinzon@gmail.com>
2025-05-16 18:08:03 -0500

    Database.py actualizado, permite insercion de datos a SQLite3

--- src/database.py
+++ src/database.py
@@ -5,15 +5,46 @@ import pandas as pd
 
 class Database:
     def __init__(self):
-        self.db_name = "src/static/db/exchangerate.db"
+        self.db_name = "bigdata_2025_7_2/src/static/db/exchangerate.db"
         self.create_database()
 
     def create_database(self):
         try:
             self.conn = sqlite3.connect(self.db_name)
             self.cursor = self.conn.cursor()
+            self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS tasas_cambio (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                fecha_captura DATETIME DEFAULT CURRENT_TIMESTAMP,
+                pais TEXT UNIQUE,
+                valor REAL
+            )
+        ''')
         except Exception as err:
             print("Fallo, capturamos error en la creacion de la base de datos")
+
+    def insert_database(self, df: pd.DataFrame):
+        """
+        Inserta los datos del DataFrame en la tabla tasas_cambio.
+        Asume que el DataFrame tiene una única fila donde las columnas son los países
+        y los valores son las tasas de cambio.
+        """
+        try:
+            if not df.empty:
+                # Iterar a través de las columnas del DataFrame (que son los países)
+                for pais, valor in df.iloc[0].items():
+                    self.cursor.execute(
+                        "INSERT OR REPLACE INTO tasas_cambio (pais, valor) VALUES (?, ?)",
+                        (pais, valor)
+                    )
+                self.conn.commit()
+                print("Datos insertados en la base de datos.")
+            else:
+                print("El DataFrame está vacío, no se insertaron datos.")
+
+        except sqlite3.Error as e:
+            print(f"Error al insertar datos en la base de datos: {e}")
+            self.conn.rollback()  # Revertir cualquier cambio en caso de error
 
     def close_database(self):
         if self.conn:
--- src/main.py
+++ src/main.py
@@ -1,11 +1,23 @@
 from datapi import DatApi
 from database import Database
+import pandas as pd
+
 def main():
 
+    df = pd.DataFrame()
     datapi = DatApi()
     database = Database()
     df = datapi.getData()
-    df.to_csv('src/static/csv/tasas_de_cambio.csv', index=False, encoding='utf-8')
+    df.to_csv('bigdata_2025_7_2/src/static/csv/tasas_de_cambio.csv', index=False, encoding='utf-8')
+    
+    nombre_tabla = "Tazas de Cambio del Dolar"
+    database.insert_database(df)
+
+    print("******** Datos insertados en la base datos tabla: {}*********".format(nombre_tabla))
+    print("******** Impresion de los datos ********")
+    print(df.shape)
+    print(df.head())
+
     database.close_database()
 
 if __name__ == "__main__":
